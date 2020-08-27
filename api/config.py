738c2981d64ebaed5554580e7606ea861b5e0bda
JSellers0 <JSellers0@Gmail.Com>
2020-08-26 22:20:42 -0400

    fuck marshmallow

--- api/config.py
+++ api/config.py
@@ -1,7 +1,6 @@
 import os
 import connexion
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
@@ -19,5 +18,3 @@ app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
-ma = Marshmallow(app)
